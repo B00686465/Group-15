f5280ffd1ca9be6e9fc78da31819082c6c5c0101
Conor <mullatof@hotmail.com>
2018-04-27 22:36:03 +0100

    About and Contact view, Bootstrap

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class PagesController extends Controller
+{
+    public function home()
+    {
+    	return view('home');
+    }
+    public function about()
+    {
+    	return view('about');
+    }
+    public function contact()
+    {
+    	return view('contact');
+    }
+}
