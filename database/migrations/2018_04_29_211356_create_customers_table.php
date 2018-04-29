d3d1ee343e41be0b1fdd01fa6f12f42c52aac679
Conor <mullatof@hotmail.com>
2018-04-29 22:58:54 +0100

    Added some views- Registration, Login

---
+++ database/migrations/2018_04_29_211356_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('customerEmail', 25);
+            $table->string('surname', 25);
+            $table->string('forename', 25);
+            $table->text('address', 25);
+            $table->string('city', 25);
+            $table->text('postcode', 25);
+            $table->string('telephone', 11);
+            $table->string('password', 15);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ tests/Feature/CreateAnAccountTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateAnAccountTest extends TestCase
+{
+    /**
+     * A basic test example.
+     * Acceptance tests
+     * @return void
+     */
+    public function test_can_create_an_account()
+    {
+        // able to get on the register page
+
+        $user = factory(User::class)->create(['username' => 'johndoe']);
+        // able to enter details
+
+        // receive notification of account complete
+
+
+    }
+}
