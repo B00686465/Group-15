ef4b737c1da25fb458415a456a854fd9f11bf9e6
Conor <mullatof@hotmail.com>
2018-04-30 21:47:18 +0100

    Fixed migrations

--- database/migrations/2018_04_30_202433_create_tickets_table.php
+++ database/migrations/2018_04_30_202433_create_tickets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTicketsTabke extends Migration
+class CreateTicketsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,7 @@ class CreateTicketsTabke extends Migration
      */
     public function up()
     {
+        // Schema is a class that we can use to define and manipulate tables. This create the tickets table
         Schema::create('tickets', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title', 255);
